9bef038990f3997699ebd801a08f300b2ec7d061
thiruaran10 <thirumaranm10@gmail.com>
2026-01-08 14:05:15 +0530

    changed the imported file name also imported another file

--- tests/test_form_submit.py
+++ tests/test_form_submit.py
@@ -1,13 +1,17 @@
+from pages.login_page import LoginPage
 from pages.home_page import HomePage
-from playwright.sync_api import expect
 
 def test_form_submit(page):
-    page.goto("https://rahulshettyacademy.com/loginpagePractise/", wait_until="networkidle")
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+
+    login = LoginPage(page)
+    login.login()  # perform login
+
     home = HomePage(page) # create POM object
     home.fill_home_page() # fill form and submit
+    
     msg = home.get_success_message() # capture success message
     print(msg)
     assert "Success" in msg
-    page.wait_for_timeout(15000)
 
 
