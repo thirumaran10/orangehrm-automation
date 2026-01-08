79d5c9a48b49cb5ab429bad17bd520fa61e9055c
thiruaran10 <thirumaranm10@gmail.com>
2026-01-08 14:03:41 +0530

    changed the imported file name and updated it

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,8 @@
 from pages.login_page import LoginPage
 
 def test_login(page):
-    page.goto("https://rahulshettyacademy.com/loginpagePractise/", wait_until="networkidle")
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     login = LoginPage(page)
-    login.login("rahulshettyacademy", "learning")
+    login.login()
     
 # for reusing the code we create a method in login_page.py and call that method here
