a61a0c342ce8a9080319e1bd0ef89a1f4b117871
thiruaran10 <thirumaranm10@gmail.com>
2026-01-08 14:01:34 +0530

    changed function name due to error occurs, added parameters

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,8 +2,8 @@ class LoginPage:
     def __init__(self, page):
         self.page = page
 
-    def test_open_browser(self,username, password):
-        self.page.fill('//input[@name="username"]','username')
-        self.page.fill('//input[@name="password"]','password')# this method used for reusing the code for login
+    def login(self, username="rahulshettyacademy", password="learning"):
+        self.page.fill('//input[@name="username"]', username)
+        self.page.fill('//input[@name="password"]', password)  # this method used for reusing the code for login
         self.page.click('//input[@type="submit"]')
 
